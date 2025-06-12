24fe11f71516ebdc865b36a190a055e29773794d
Flip <117127775+FlipSkateHd@users.noreply.github.com>
2025-06-11 22:35:02 -0300

    Protótipo e teste adicionados

--- HashCrack.py
+++ HashCrack.py
@@ -1,14 +1,21 @@
-import sys 
-
 import hashlib  
 
-def ler_arquivo(file):
+def lerArquivo(file):
     with open(file, 'r') as f:
         return f.read().splitlines()
 
-hashes = []
-Buffer = 65536
-for palavra in ler_arquivo("wordlist.txt"):
-    hashes.append(hashlib.sha256(palavra))
+def hashList():
+    hashes = []
+    for palavra in lerArquivo("wordlist.txt"):
+        hash_obj = hashlib.sha256(palavra.encode('utf-8'))
+        hashes.append(hash_obj.hexdigest())
+    return hashes
 
-print(hashes)
+def comparaHashes(hashes,alvo):
+    for hash in hashes:
+        if hash == alvo:
+            print("Hash encontrado na wordlist: ", hash)
+    
+print(hashList())
+
+comparaHashes(hashes= hashList(),alvo= input("Digite a sua hash: "))
---
+++ teste.py
@@ -0,0 +1,11 @@
+import hashlib
+
+##  COMPARAÇÃO DE OBJETO DE HASH COM VALOR DE HASH
+
+objeto_hash = hashlib.sha256(b"senhasecreta")
+print(objeto_hash)
+## Esse aqui retorna o ponteiro da memória para o objeto desse hash que foi feito
+
+## Ao pegar o objeto e usar o "digest", obtém o hash calculado em hexadecima, normalmente usado em verificações
+hash_legal = objeto_hash.hexdigest()
+print(hash_legal)
