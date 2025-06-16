42d5e887f8c4c75d65aea03714bf8b6d0ac17d61
flipskatehd <flipskatehd@gmail.com>
2025-06-16 15:18:42 -0300

    Descição aprimorada dos testes com hash feitos

--- teste.py
+++ teste.py
@@ -3,9 +3,9 @@ import hashlib
 ##  COMPARAÇÃO DE OBJETO DE HASH COM VALOR DE HASH
 
 objeto_hash = hashlib.sha256(b"senhasecreta")
-print(objeto_hash)
+print("Hash feita na memória com ponteiro: ", objeto_hash)
 ## Esse aqui retorna o ponteiro da memória para o objeto desse hash que foi feito
 
 ## Ao pegar o objeto e usar o "digest", obtém o hash calculado em hexadecimal, normalmente usado em verificações
 hash_legal = objeto_hash.hexdigest()
-print(hash_legal)
+print("Hash em formato legível: ", hash_legal)
